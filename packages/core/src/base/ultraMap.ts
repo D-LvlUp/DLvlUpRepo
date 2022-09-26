88cff853b2428f1ffbf5913c78375658a9ccdf19
Rafael Cedeño <Rafaelcv7@outlook.com>
2022-09-26 00:08:59 -0400

    initial commit

---
+++ packages/core/src/base/ultraMap.ts
@@ -0,0 +1,23 @@
+import { ClassTransformOptions, plainToInstance } from "class-transformer";
+import { validate, ValidatorOptions } from "class-validator";
+const consola = require("consola");
+
+const ctOptions: ClassTransformOptions = {
+  excludeExtraneousValues: true,
+};
+
+const vOptions: ValidatorOptions = {
+  validationError: { target: false },
+};
+
+export function ultraMap<T>(type: new () => T, props?: object): T {
+  const ins: any = plainToInstance(type, props, ctOptions);
+
+  validate(ins, vOptions).then((errors) => {
+    if (errors.length > 0) {
+      consola.error(errors);
+      throw new Error("Validation for class creation did not succeed.");
+    }
+  });
+  return ins;
+}
